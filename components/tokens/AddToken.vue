5361909022d1db5583f5fe33a49928aee5b0c1ba
Ephrem Demelash <ephrem@addissoftware.com>
2023-02-20 16:06:19 +0300

    feat: improve token CRUD and comp structure

--- components/tokens/AddToken.vue
+++ components/tokens/AddToken.vue
@@ -1,117 +1,103 @@
-<!-- 
-    The component called by Add and Edit is the same, and only has address field.
-    The data is passed to the component by the parent component.
-    The component is a modal form controlled by visisble prop.
- -->
 <template>
-    <div class="add-token">
-        <a-modal
-        :title="title"
-        :visible="visible"
-        @cancel="onCancel"
-        @ok="onOk"
-        :confirm-loading="confirmLoading"
-        >
-        <a-form :form="form">
-            <a-form-item label="Address">
-            <a-input
-                v-decorator="[
-                'address',
-                {
-                    rules: [
-                    { required: true, message: 'Please input your address!' },
-                    ],
-                },
-                ]"
-                placeholder="Address"
-            />
-            </a-form-item>
-        </a-form>
-        </a-modal>
-    </div>
+  <div class="add-token">
+    <a-form :form="form" @submit="handleSubmit">
+      <a-form-item label="Token Address">
+        <a-input
+          v-decorator="[
+            'address',
+            {
+              rules: [
+                { required: true, message: 'Please input token address!' },
+              ],
+            },
+          ]"
+        />
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" html-type="submit" :loading="loading">
+          Submit
+        </a-button>
+      </a-form-item>
+    </a-form>
+  </div>
 </template>
 
-
 <script>
-export default {
-    name: 'AddToken',
-    props: {
-        visible: {
-            type: Boolean,
-            required: true,
-        },
-        title: {
-            type: String,
-            required: true,
-        },
-        data: {
-            type: Object,
-            required: true,
-        },
+import { Form } from 'ant-design-vue'
 
-        onCancel: {
-            type: Function,
-            required: true,
-        },
-        onOk: {
-            type: Function,
-            required: true,
-        },
+export default {
+  name: 'NuxtAddToken',
+  components: {
+    'a-form': Form,
+  },
+  props: {
+    onSubmit: {
+      type: Function,
+      default: () => {},
     },
-    data() {
-        return {
-            form: this.$form.createForm(this),
-            confirmLoading: false,
+    formData: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+      // populate form with data
+
+      form: this.$form.createForm(this),
+      loading: false,
+    }
+  },
+
+  // check if form data has changed and populate form
+  watch: {
+    formData: {
+      handler: function (val, oldVal) {
+        if (val && oldVal && val.address !== oldVal.address) {
+          this.form.setFieldsValue(val)
         }
+        if (!val || !val.address) {
+          this.form.resetFields()
+        }
+
+        return val
+      },
+      deep: true,
+      immediate: true,
     },
+  },
+
+  mounted() {
+    this.form.setFieldsValue(this.formData)
+  },
+
+  unmounted() {
+    this.form.resetFields()
+  },
+
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault()
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          this.loading = true
+          this.onSubmit({ ...this.formData, ...values })
+        }
+      })
+
+      this.loading = false
+      this.resetForm()
+    },
+
+    resetForm() {
+      this.form.resetFields()
+    },
+  },
 }
 </script>
 
-
 <style scoped>
 .add-token {
-    width: 100%;
-}
-
-.add-token .ant-modal-body {
-    padding: 24px 24px 0;
-}
-
-.add-token .ant-modal-footer {
-    padding: 10px 24px 10px;
-}
-
-.add-token .ant-modal-footer .ant-btn {
-    margin-right: 8px;
-}
-
-.add-token .ant-modal-footer .ant-btn:last-child {
-    margin-right: 0;
-}
-
-.add-token .ant-modal-header {
-    padding: 16px 24px;
-}
-
-.add-token .ant-modal-header .ant-modal-title {
-    font-size: 16px;
-    font-weight: 500;
-}
-
-.add-token .ant-modal-header .ant-modal-close-x {
-    top: 16px;
-    right: 16px;
-}
-
-.add-token .ant-modal-content {
-    border-radius: 4px;
-}
-
-.add-token .ant-modal-content .ant-modal-body {
-    padding: 24px 24px 0;
-}
-
-.add-token .ant-modal-content .ant-modal-footer {
-    padding: 10px 24px 10px;
+  width: 100%;
 }
 </style>
