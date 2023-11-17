2685cd1164b3a6f6ed7e7600ecb90a12cc624701
Ephrem Demelash <demelashephrem@gmail.com>
2023-11-17 05:06:41 +0300

    Refactor: Swap component to use SwapToken component

--- pages/Swap.vue
+++ pages/Swap.vue
@@ -1,121 +1,18 @@
+<!-- eslint-disable vue/first-attribute-linebreak -->
 <template>
-  <div>
-    <a-row type="flex" flex justify="space-around" align="middle">
-      <a-col :span="9">
-        <h1>Swap</h1>
-
-        <div class="filter">
-          <a-form-item label="Wallet: ">
-            <a-select
-              placeholder="Select Wallet"
-              style="width: 100%"
-              @change="handleWalletChange"
-            >
-              <a-select-option
-                v-for="wallet in wallets"
-                :key="wallet._id"
-                :value="wallet._id"
-              >
-                {{ wallet.name }}
-              </a-select-option>
-            </a-select>
-          </a-form-item>
-        </div>
-      </a-col>
-    </a-row>
-
-    <a-row type="flex" flex justify="center" align="middle">
-      <a-col :span="8">
-        <a-form
-          v-if="selectedWallet"
-          :form="form"
-          :layout="layout"
-          :label-col="labelCol"
-          :wrapper-col="wrapperCol"
-          @submit="handleSubmit"
-        >
-          <a-form-item label="Token 1">
-            <a-select
-              v-decorator="[
-                'token1',
-                {
-                  rules: [
-                    { required: true, message: 'Please select a token!' },
-                  ],
-                },
-              ]"
-              placeholder="Select a token"
-              style="width: 100%"
-            >
-              <a-select-option
-                v-for="token in tokens"
-                :key="token._id"
-                :value="token.address"
-              >
-                {{ token.name }} - {{ token.balance }}
-              </a-select-option>
-            </a-select>
-          </a-form-item>
-          <a-form-item label="Token 1 Amount">
-            <a-input-number
-              v-decorator="[
-                'token1Amount',
-                {
-                  rules: [
-                    { required: true, message: 'Please input token amount!' },
-                  ],
-                },
-              ]"
-              :min="0"
-              :max="1000000000"
-              :step="0.01"
-            />
-          </a-form-item>
-
-          <a-form-item label="Token 2">
-            <a-select
-              v-decorator="[
-                'token2',
-                {
-                  rules: [
-                    { required: true, message: 'Please select a token!' },
-                  ],
-                },
-              ]"
-              placeholder="Select a token"
-              style="width: 100%"
-            >
-              <a-select-option
-                v-for="token in tokens"
-                :key="token._id"
-                :value="token.address"
-              >
-                {{ token.name }} - {{ token.balance }}
-              </a-select-option>
-            </a-select>
-          </a-form-item>
-
-          <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
-            <a-button type="primary" html-type="submit" :loading="loading">
-              Submit
-            </a-button>
-          </a-form-item>
-        </a-form>
-      </a-col>
-    </a-row>
-  </div>
+  <SwapComponent :wallets="wallets" :tokens="tokens" :loading="loading" :on-submit="handleSubmit"
+    :price="price" />
 </template>
 
 <script>
-import { Form, Select, InputNumber } from 'ant-design-vue'
+import SwapComponent from '../components/swap/SwapToken.vue'
 
 export default {
   name: 'NuxtSwap',
   components: {
-    'a-form': Form,
-    'a-select': Select,
-    'a-input-number': InputNumber,
+    SwapComponent,
   },
+  layout: 'bot',
 
   async asyncData({ $axios }) {
     const wallets = await $axios.get('/api/accounts')
@@ -128,7 +25,7 @@ export default {
       form: this.$form.createForm(this),
       wallets: [],
       tokens: [],
-      selectedWallet: '',
+      price: NaN,
 
       layout: 'horizontal',
       labelCol: { span: 5 },
@@ -137,93 +34,61 @@ export default {
     }
   },
 
-  watch: {
-    selectedWallet: {
-      handler: function (val, oldVal) {
-        this.form.resetFields()
-
-        if (val && val !== oldVal) {
-          this.$axios.get(`/api/accounts/${val}/tokens`).then((res) => {
-            this.tokens = res.data
-          })
-        }
-      },
-      immediate: true,
-    },
-  },
-
-  mounted() {
-    if (this.selectedWallet) {
+  methods: {
+    handleSubmit(values) {
+      this.loading = true
       this.$axios
-        .get(`/api/accounts/${this.selectedWallet}/tokens`)
+        .post(`/api/uniswap/swap`, values)
         .then((res) => {
-          this.tokens = res.data
+          if (res.data === 'Failed')
+            this.$message.error('Swap Failed')
+          else this.$message.success('Swapped Successfully: ' + res.data.transactionHash)
+        })
+        .catch((err) => {
+          this.$message.error('Swap failed' + err.message)
+        })
+        .finally(() => {
+          this.loading = false
+        })
+
+    },
+
+    fetchRate(token1, token2) {
+      if (token1 === token2) return 1;
+
+      if (!token1 || !token2) return NaN;
+
+      this.loading = true
+      this.$axios
+        .get(
+          `/api/uniswap/exchange-rate/${token1}/${token2}`
+        )
+        .then((res) => {
+          this.price = +(res.data)
+        })
+        .catch((err) => {
+          this.$message.error("Can't get price: " + err.message)
+        })
+        .finally(() => {
+          this.loading = false
         })
     }
-  },
-  methods: {
-    handleSubmit(e) {
-      e.preventDefault()
-      this.form.validateFields((err, values) => {
-        if (!err) {
-          this.loading = true
-          this.$axios
-            .post(`/api/uniswap/swap`, {
-              ...values,
-              wallet: this.selectedWallet,
-            })
-            .then((res) => {
-              if (res.data === 'Failed')
-                this.$message.error('Swap ' + err)
-              else this.$message.success('Swap ' + res.data)
-            })
-            .catch((err) => {
-              this.$message.error('Swap failed' + err.message)
-            })
-            .finally(() => {
-              this.loading = false
-            })
-        }
-      })
-    },
 
-    handleWalletChange(value) {
-      this.selectedWallet = value
-    },
+  },
+
+  fetchTokens(walletId) {
+    this.loading = true
+    this.$axios
+      .get(`/api/accounts/${walletId}/tokens`)
+      .then((res) => {
+        this.tokens = res.data
+      })
+      .catch((err) => {
+        this.$message.error('Error getting tokens: ' + err.message)
+      })
+      .finally(() => {
+        this.loading = false
+      })
   },
 }
 </script>
-
-<style scoped>
-.filter {
-  margin-bottom: 20px;
-}
-
-.form {
-  margin-top: 20px;
-}
-
-.form-item {
-  margin-bottom: 0;
-}
-
-.form-item-label {
-  text-align: left;
-}
-
-.form-item-control {
-  margin-left: 0;
-}
-
-.form-item-control-input {
-  width: 100%;
-}
-
-.form-item-control-input-content {
-  width: 100%;
-}
-
-.form-item-control-input-content input {
-  width: 100%;
-}
-</style>
